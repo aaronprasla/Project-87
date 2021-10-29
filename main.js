e8aef63b7983444ed317994aa6796c0a8aab05dd
aaronprasla <52900151+aaronprasla@users.noreply.github.com>
2021-10-28 19:05:36 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,63 +1,49 @@
-canvas = document.getElementById('myCanvas');
 
-block_y = 1;
-block_x = 1;
+// Create canvas variable
+ block_y=1;
+ block_x=1;
 
 block_image_width = 350;
 block_image_height = 430;
 
-var block_image_object = "";
+var block_image_object= "";
 
-function new_image(get_image){
-fabric.Image.fromURL(get_image, function(Img){ 
-	block_image_object = Img;
-	block_image_object.scaleToWidth(block_image_width); 
-	block_image_object.scaleToHeight(block_image_height); 
-	block_image_object.set({ 
-		top:block_y, 
-		left:block_x
-});
-canvas.add(block_image_object);
-});
+function new_image(get_image)
+{
+	// to upload images
 }
+
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
 keyPressed = e.keyCode;
-
 console.log(keyPressed);
 
-	if(keyPressed == '82')
+	if(keyPressed == '?') // add appropriate keycode
 	{
-		new_image("rr1.png");
-		console.log("r");
+		// upload red ranger
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		new_image("rr1.png");
-		console.log("");
+		// upload green ranger
 	}
 	
 	if(keyPressed == '89')
 	{
-		block_x = 350;
-		new_image("rr1.png");
-		console.log("r");
+		block_x =350;
+		// upload yellow ranger
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		new_image("rr1.png");
-		console.log("r");
+		// upload pink ranger
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-		new_image("rr1.png");
-		console.log("r");
-
+	// upload blue ranger
 	}
 	
 }
